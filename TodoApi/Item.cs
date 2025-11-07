d697455ecba7bfa938a53b68f8b48d3a1b7ca00d
miri-gamliel <mirigam0049@gmail.com>
2025-11-07 02:24:24 +0200

    first commit

---
+++ TodoApi/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoApi;
+public class CreateItemDto
+{
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
+public partial class Item
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public bool? IsComplete { get; set; }
+}
---
+++ TodoApi/Program.cs
@@ -0,0 +1,65 @@
+using TodoApi;
+using Pomelo.EntityFrameworkCore.MySql;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()    
+              .AllowAnyMethod()   
+              .AllowAnyHeader();   
+    });
+});
+
+var connectionString = builder.Configuration.GetConnectionString("ToDoDB");
+
+builder.Services.AddDbContext<ToDoDbContext>(options =>
+    options.UseMySql(connectionString,ServerVersion.AutoDetect(connectionString)));
+
+var app = builder.Build();
+
+app.UseCors("AllowAll");
+
+app.MapGet("/todos", (ToDoDbContext db) => 
+{
+     return db.Items.ToList();
+});
+
+app.MapPost("/todos",(CreateItemDto item,ToDoDbContext db)=>
+{
+    Item newItem=new Item{Name=item.Name,IsComplete=item.IsComplete};
+    db.Items.Add(newItem);
+    db.SaveChanges();
+    return Results.Created($"/todos/{newItem.Id}", newItem);
+});
+
+app.MapPut("/todo/{id}",(int id,bool IsComplete,ToDoDbContext db)=>
+{
+    var item=db.Items.Find(id);
+    if(item==null)
+        return Results.NotFound();
+    item.IsComplete=IsComplete;
+    db.SaveChanges();
+    return Results.NoContent();
+});
+
+app.MapDelete("/todo/{id}",(int id,ToDoDbContext db)=>
+{
+    var item=db.Items.Find(id);
+    if(item==null)
+        return Results.NotFound();
+    db.Items.Remove(item);    
+    db.SaveChanges();
+    return Results.Ok(item);
+});
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.Run();
