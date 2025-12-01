0bf82e5f2c0862459dfb7ae817adc1615e26408c
miri-gamliel <mirigam0049@gmail.com>
2025-12-01 16:03:11 +0200

    second!

---
+++ ToDoListReact/src/service.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+const apiUrl = "http://localhost:5130"
+axios.defaults.baseURL = apiUrl;
+
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.error("Axios Error:", {
+      url: error.config?.url,
+      method: error.config?.method,
+      status: error.response?.status,
+      message: error.message,
+    });
+
+    return Promise.reject(error);
+  }
+);
+
+
+export default {
+  getTasks: async () => {
+    const result = await axios.get(`/todos`)    
+    return result.data;
+  },
+
+  addTask: async(name)=>{
+    console.log('addTask', name)
+    const result=await axios.post(`/todos`,{"name":name,"isComplete":false});
+    return result.data;
+  },
+
+  setCompleted: async(id, isComplete)=>{
+    console.log('setCompleted', {id, isComplete})
+    const result= await axios.put(`/todo/${id}?isComplete=${isComplete}`)
+    return result.data;
+  },
+
+  deleteTask:async(id)=>{
+    console.log('deleteTask')
+    await axios.delete(`/todo/${id}`)
+  }
+};
